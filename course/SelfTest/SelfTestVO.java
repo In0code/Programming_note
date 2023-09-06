cc0d4fb5ac856d872cbf2f0bdee07fcd50016fe5
In0code <137425054+In0code@users.noreply.github.com>
2023-09-06 09:06:19 +0900

    Add files via upload

---
+++ course/SelfTest/SelfTestVO.java
@@ -0,0 +1,18 @@
+package SelfTest;
+
+public class SelfTestVO {
+	private String name;
+	private String address;
+	private int age;
+
+	public SelfTestVO() {
+
+	}
+
+	public SelfTestVO(String name, String address, int age) {
+		this.name = name;
+		this.address = address;
+
+	}
+
+}
