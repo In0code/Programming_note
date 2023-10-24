1264e67b563b39dc79a3505a8b85b16aa13b45cd
In0code <137425054+In0code@users.noreply.github.com>
2023-10-24 09:28:23 +0900

    Add files via upload

--- JSP/java/kr/co/sist/member/dao/BoardDAO.java
+++ JSP/java/kr/co/sist/member/dao/BoardDAO.java
@@ -27,7 +27,14 @@ public class BoardDAO {
 		return bDAO;
 	}//getInstance
 	
-	public int totalCount() throws SQLException{
+	/**
+	 * 10/23 Dynamic Query로 변경
+	 * @param brVO
+	 * @return
+	 * @throws SQLException
+	 */
+	/* public int totalCount( ) throws SQLException{ */
+		public int totalCount( BoardRangeVO brVO ) throws SQLException{
 		int totalCount=0;
 		
 		DbConnection db = DbConnection.getInstance();
@@ -43,10 +50,25 @@ public class BoardDAO {
 			con=db.getConn("jdbc/dbcp");
 		//4.
 			StringBuilder selectCnt=new StringBuilder();
-			selectCnt.append("	select count(*) cnt from web_member	");
+			selectCnt.append("	select count(*) cnt from web_member		");
+			
+			if(brVO.getKeyword() != null && ! "".equals(brVO.getKeyword() )&& !"null".equals(brVO.getKeyword())){
+				String field="info";
+				if("1".equals(brVO.getField() )) {
+					field="id";
+				}//end if
+				if("2".equals(brVO.getField() )) {
+					field="addr1";
+				}//end if
+				
+				selectCnt.append("where ").append(field).append(" like '%'||?||'%' ");
+			}//end if
+			
 			pstmt=con.prepareStatement(selectCnt.toString());
 		//5.
-			
+			if(brVO.getKeyword() != null && ! "".equals(brVO.getKeyword() )&& !"null".equals(brVO.getKeyword())){
+				pstmt.setString(1, brVO.getKeyword());
+			}//end if
 		//6.
 			rs=pstmt.executeQuery();
 			
@@ -80,19 +102,37 @@ public class BoardDAO {
 		//2. DataSource 얻기
 		//3. Connection 얻기
 			con=db.getConn("jdbc/dbcp");
-		//4. 쿼리문 생성 객체 얻기
+		//4. 쿼리문 생성 객체 얻기 => 검색 키워드와 검색 field에 따라 Dynamic Query로 변경
 			StringBuilder selectMember=new StringBuilder();
 			selectMember
 			.append("	select id, name, birth, tel, email, gender , input_date			")
 			.append("	from(select id, name, birth, tel, email, gender , input_date	")
 			.append("	,row_number() over( order by input_date desc) rnum				")
-			.append("	from web_member)												")
-			.append("	where rnum between ? and ?									");
+			.append("	from web_member												");
+			
+			if(brVO.getKeyword() != null && !"".equals(brVO.getKeyword() )&& !"null".equals(brVO.getKeyword())) {
+				String field="info";
+				if("1".equals(brVO.getField())) {
+					field="id";
+				}//end if
+				if("2".equals(brVO.getField())) {
+					field="addr1";
+				}//end if
+				
+				selectMember.append("where ").append(field).append(" like '%'||?||'%' ");
+			}//end if
+			
+			selectMember.append(")	where rnum between ? and ?					");
 			
 			pstmt=con.prepareStatement(selectMember.toString());
 		//5. 바인드 변수 값 설정
-			pstmt.setInt(1, brVO.getStartNum());
-			pstmt.setInt(2, brVO.getEndNum());
+			int bindCnt=1;
+			if(brVO.getKeyword() != null && !"".equals(brVO.getKeyword() )&& !"null".equals(brVO.getKeyword())) {
+			pstmt.setString(bindCnt++, brVO.getKeyword());	
+			}//end if
+			
+			pstmt.setInt(bindCnt++, brVO.getStartNum());
+			pstmt.setInt(bindCnt++, brVO.getEndNum());
 		//6. 쿼리문 실행 후 결과 얻기
 			rs=pstmt.executeQuery();
 			WebMemberVO wmVO=null;
--- JSP/java/kr/co/sist/member/vo/BoardRangeVO.java
+++ JSP/java/kr/co/sist/member/vo/BoardRangeVO.java
@@ -3,15 +3,9 @@ package kr.co.sist.member.vo;
 public class BoardRangeVO {
 
 	private int startNum, endNum;
+	private String field, keyword;
 
 	public BoardRangeVO() {
-		super();
-	}
-
-	public BoardRangeVO(int startNum, int endNum) {
-		super();
-		this.startNum = startNum;
-		this.endNum = endNum;
 	}
 
 	public int getStartNum() {
@@ -30,10 +24,28 @@ public class BoardRangeVO {
 		this.endNum = endNum;
 	}
 
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
 	@Override
 	public String toString() {
-		return "BoardRangeVO [startNum=" + startNum + ", endNum=" + endNum + "]";
+		return "BoardRangeVO [startNum=" + startNum + ", endNum=" + endNum + ", field=" + field + ", keyword=" + keyword
+				+ "]";
 	}
+
 	
 	
 	
