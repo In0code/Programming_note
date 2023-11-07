a04c3472840cc3b284a18779b95110b17904ec00
In0code <137425054+In0code@users.noreply.github.com>
2023-11-07 10:21:09 +0900

    Add files via upload

---
+++ Framework/mybatis_prj/src/main/java/day1106/DeptDAO.java
@@ -0,0 +1,25 @@
+package day1106;
+
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.sist.dao.MybatisHandler;
+import kr.co.sist.vo.DeptVO;
+
+public class DeptDAO {
+
+	public void insertDept(DeptVO dVO) throws PersistenceException {
+		int cnt=0;
+		//1. MyBatis Handler 얻기
+		MybatisHandler mbh=MybatisHandler.getInstance();
+		SqlSession ss=mbh.getMyBatisHandler("kr/co/sist/dao/mybatis-config.xml", true);
+		//2. DB 작업 수행
+		cnt=ss.insert("kr.co.sist.dept.insertDept",dVO);
+		/*
+		 * if(cnt ==1 ) { ss.commit(); }//end if
+		 */		
+		//3. Handler 닫기
+		ss.close();
+	}//insertDept
+	
+}//class
---
+++ Framework/mybatis_prj/src/main/java/day1106/TestMyBatis.java
@@ -0,0 +1,30 @@
+package day1106;
+
+import org.apache.ibatis.exceptions.PersistenceException;
+
+import kr.co.sist.vo.DeptVO;
+
+public class TestMyBatis {
+	
+	public void addDept() {
+		DeptVO dVO=new DeptVO();
+		dVO.setDeptno(98);
+		dVO.setDname("개발");
+		dVO.setLoc("서울");
+		
+		DeptDAO dDAO=new DeptDAO();
+		try {
+			dDAO.insertDept(dVO);
+			System.out.println("추가성공");
+		}catch(PersistenceException pe) {
+			pe.printStackTrace();
+			System.out.println("추가실패");
+		}//end catch
+	}//addDept
+	
+	public static void main(String[] args) {
+		TestMyBatis tmb=new TestMyBatis();
+		tmb.addDept();
+	}//main
+
+}//class
