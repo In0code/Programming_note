567857525aed757091432e13572837cd34881537
In0code <137425054+In0code@users.noreply.github.com>
2023-12-07 16:11:19 +0900

    Add files via upload

--- sef_practice/retro/src/main/java/kr/co/sist/admin/controller/AdminProductController.java
+++ sef_practice/retro/src/main/java/kr/co/sist/admin/controller/AdminProductController.java
@@ -1,22 +1,47 @@
 package kr.co.sist.admin.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.sist.admin.domain.AdminProductDomain;
+import kr.co.sist.admin.service.AdminProductService;
+import kr.co.sist.common.BoardRangeVO;
+
+
 @Controller
 public class AdminProductController {
 	
+	private AdminProductService aps=AdminProductService.getInstance();
+	
 	@RequestMapping("/admin/product_managing.do")
-	public String productManage(HttpServletRequest request, Model model) {
+	public String productManage(HttpServletRequest request,BoardRangeVO brVO, Model model) {
 		
-		model.addAttribute("method",request.getMethod());
+		
+		
+		/*
+		 * String keyword=request.getParameter("keyword"); String
+		 * field=request.getParameter("field");
+		 */
+		
+		brVO.setKeyword("싱숭생숭연");
+//		brVO.setField(field);
+		
+		List<AdminProductDomain> productList=aps.searchAllProduct(brVO);
+		
+		model.addAttribute("prductList",productList);
+		
 		
 		return "admin/product_managing";
 	}//productManage
 		
+	
+	
+	
 	@RequestMapping("/admin/productDetail_managing.do")
 	public String productManageDetail(HttpServletRequest request, Model model) {
 		
