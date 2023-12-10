599953e3f82ccb22265e87254d1f3d63e0ed5dfd
In0code <137425054+In0code@users.noreply.github.com>
2023-12-11 00:03:31 +0900

    Add files via upload

--- sef_practice/retro/src/main/java/kr/co/sist/user/dao/ProductDAO.java
+++ sef_practice/retro/src/main/java/kr/co/sist/user/dao/ProductDAO.java
@@ -1,5 +1,43 @@
 package kr.co.sist.user.dao;
 
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.sist.common.dao.MybatisHandler;
+import kr.co.sist.user.domain.ProductDomain;
+
 public class ProductDAO {
 
-}
+	private static ProductDAO pDAO;
+	private String configPath;
+
+	private ProductDAO() {
+		configPath = "kr/co/sist/common/dao/mybatis-config.xml";
+	}
+
+	public static ProductDAO getInstance() {
+		if (pDAO == null) {
+			pDAO = new ProductDAO();
+		} // end if
+		return pDAO;
+	}// getInstance
+
+	/**
+	 * 카테고리 검색
+	 * @return
+	 * @throws PersistenceException
+	 */
+	public ProductDomain selectCategory() throws PersistenceException {
+		ProductDomain pd= null;
+
+		MybatisHandler mbh=MybatisHandler.getInstance();
+		SqlSession ss=mbh.getMyBatisHandler(configPath,false);
+		pd=ss.selectOne("user.product.selectCategory");
+		
+		mbh.closeHandler(ss);
+			
+		return pd;
+	}//selectCategory
+
+
+}// class
---
+++ sef_practice/retro/src/main/java/kr/co/sist/user/domain/ProductDomain.java
@@ -0,0 +1,12 @@
+package kr.co.sist.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProductDomain {
+	private String category, category2, category3, ccode,c2code,c3code;
+}//class
