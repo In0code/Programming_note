aa329536e6b9345c5fbaba9584c3b20d44cac964
In0code <137425054+In0code@users.noreply.github.com>
2023-12-16 20:25:43 +0900

    Add files via upload

--- sef_practice/retro/src/main/java/kr/co/sist/user/controller/sales/SalesController.java
+++ sef_practice/retro/src/main/java/kr/co/sist/user/controller/sales/SalesController.java
@@ -1,13 +1,33 @@
 package kr.co.sist.user.controller.sales;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.sist.user.service.sales.SalesService;
+import kr.co.sist.user.vo.sales.SalesVO;
 
 @Controller
 public class SalesController {
 
 	@Autowired
 	private SalesService sService;
+	
+	@GetMapping("/user_sales_frm.do")
+	public String userSalesFrm(HttpSession session, Model model) {
+		return "user/user_mypage/user_sales_frm";
+	}
+	
+	@GetMapping("/user_sales.do")
+	public String userSalesProcess(HttpSession session, char flag, Model model) {
+		String id = (String)session.getAttribute("id");
+		SalesVO sVO = new SalesVO(id, flag);
+		
+		model.addAttribute("saleList", sService.searchSales(sVO));
+		
+		return "user/user_mypage/user_sales_process";
+	}
 }
--- sef_practice/retro/src/main/java/kr/co/sist/user/service/sales/SalesService.java
+++ sef_practice/retro/src/main/java/kr/co/sist/user/service/sales/SalesService.java
@@ -1,13 +1,47 @@
 package kr.co.sist.user.service.sales;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.sist.user.dao.sales.SalesDAO;
+import kr.co.sist.user.domain.sales.SalesDomain;
+import kr.co.sist.user.vo.sales.SalesVO;
 
 @Service
 public class SalesService {
 
 	@Autowired
 	private SalesDAO sDAO;
+	
+	public List<SalesDomain> searchSales(SalesVO sVO) {
+		List<SalesDomain> list = null;
+
+		switch (sVO.getFlag()) {
+		case 'A':
+			list = searchAllSales(sVO.getId());
+			break;
+		case 'O':
+			list = searchOnSales(sVO.getId());
+			break;
+		case 'F':
+			list = searchFinishedSales(sVO.getId());
+			break;
+		}
+		
+		return list;
+	}
+	
+	public List<SalesDomain> searchAllSales(String id) {
+		return sDAO.selectAllSales(id);
+	}
+	
+	public List<SalesDomain> searchOnSales(String id) {
+		return sDAO.selectOnSales(id);
+	}
+	
+	public List<SalesDomain> searchFinishedSales(String id) {
+		return sDAO.selectFinishedSales(id);
+	}
 }
